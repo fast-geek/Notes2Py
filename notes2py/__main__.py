755a61a086f11b5a6a03391b9cfaeaa769bec04e
lav <me@lavn.ml>
2022-05-22 09:44:47 +0300

    fixed note removing

--- notes2py/__main__.py
+++ notes2py/__main__.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 
 from PyQt6 import QtWidgets
-from tinydb import TinyDB
+from tinydb import TinyDB, Query, where
 
 from notes2py.ui.main import Ui_MainWindow
 
@@ -21,28 +21,46 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def add_new_note(self):
         self.notes_list.addItem("Новая заметка")
         self.session.insert(
-            {"name": "Новая заметка", "text": "Текст заметки"}
+            {
+                "id": self.notes_list.count() - 1,
+                "name": "Новая заметка",
+                "text": "Текст заметки"
+            }
         )
         self.notes_list.setCurrentRow(self.notes_list.count() - 1)
 
     def save_note_handler(self):
-        self.session.update(
-            {
-                "text": self.textEdit.toPlainText(),
-                "name": self.name_field.text(),
-            },
-            doc_ids=[self.notes_list.currentRow() + 1],
-        )
-        self.notes_list.clear()
-        self.notes_list.addItems([item['name'] for item in db_session.all()])
+        if self.notes_list.currentRow() != -1:
+            self.session.update(
+                {
+                    "text": self.textEdit.toPlainText(),
+                    "name": self.name_field.text(),
+                },
+                cond=where("id") == self.notes_list.currentRow()
+            )
+            self.notes_list.clear()
+            self.notes_list.addItems([item['name'] for item in self.session.all()])
 
     def delete_note_handler(self):
-        self.session.remove(doc_ids=[self.notes_list.currentIndex().row()])
+        Note = Query()
+        self.session.remove(Note.id == int(self.notes_list.currentRow()))
+        items = [{"id": index, "name": item['name'], "text": item['text']} for index, item in
+                 enumerate(self.session.all())]
+        self.session.truncate()
+        self.session.insert_multiple(items)
         self.notes_list.takeItem(self.notes_list.currentIndex().row())
 
     def show_note_handler(self):
-        self.textEdit.setText(self.session.get(doc_id=self.notes_list.currentRow() + 1)["text"])
-        self.name_field.setText(self.session.get(doc_id=self.notes_list.currentRow() + 1)["name"])
+        Note = Query()
+        self.save_note.setVisible(True)
+        self.delete_note.setVisible(True)
+        self.name_field.setVisible(True)
+        self.name_label.setVisible(True)
+        self.textEdit.setVisible(True)
+        current_id = int(self.session.search(Note.id == int(self.notes_list.currentRow()))[0]['id'])
+        note = self.session.search(Note.id == current_id)[0]
+        self.textEdit.setText(note["text"])
+        self.name_field.setText(note["name"])
 
 
 def main():
@@ -51,8 +69,14 @@ def main():
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow(db_session)
     window.notes_list.addItems([item['name'] for item in db_session.all()])
+    window.save_note.setVisible(False)
+    window.delete_note.setVisible(False)
+    window.name_field.setVisible(False)
+    window.name_label.setVisible(False)
+    window.textEdit.setVisible(False)
     window.show()
     app.exec()
+    db_session.close()
 
 
 if __name__ == "__main__":
